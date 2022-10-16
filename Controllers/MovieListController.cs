1dbe544af96c90e3d77af1b5d9e37e7f8d06242c
Michael Kolyadin <mkolyadin@bi-telco.com>
2022-10-16 11:23:47 +0300

    wip

---
+++ Controllers/MovieListController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class MovieListController : Controller
+    {
+        // 
+        // GET: /MovieList/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /MovieList/Welcome/ 
+
+        public IActionResult Welcome( int ID = 1, string name = "friend", int numTimes = 1)
+        {
+            ViewData["Id"] = ID;
+            ViewData["Message"] = "Hello " + name + ' ' + ID;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+    }
+}
